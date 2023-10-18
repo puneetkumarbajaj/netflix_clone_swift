229a7cd09aeb6cbb4e696c1315650a9196c24729
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-18 19:38:33 -0400

    setting up the header. alpha v1

---
+++ Netflix_Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,44 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix_Clone
+//
+//  Created by Puneet Bajaj on 10/18/23.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+
+    private func addGradient(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError()
+    }
+}
