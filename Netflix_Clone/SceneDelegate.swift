50a7ab826ec52c4c6157d8800f1a54222b9df1ea
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-18 17:21:28 -0400

    setting background colors and icons to test the functionality of tab bar

--- Netflix_Clone/SceneDelegate.swift
+++ Netflix_Clone/SceneDelegate.swift
@@ -14,6 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windownScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windownScene.coordinateSpace.bounds)
+        window?.windowScene = windownScene
+        window?.rootViewController = MainTabBarViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
