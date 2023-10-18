59a9ca7179c3d15d46e46e85526d152a89ac7b88
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-18 16:40:28 -0400

    Initial Commit

---
+++ Netflix_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone
+//
+//  Created by Puneet Bajaj on 10/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
