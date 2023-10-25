72d36b0ac488436cd7f9709033a6c904ed6d6d98
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-25 18:08:32 -0400

    Upcoming page done

---
+++ Netflix_Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix_Clone
+//
+//  Created by Puneet Bajaj on 10/25/23.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+}
