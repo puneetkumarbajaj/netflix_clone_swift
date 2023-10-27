9a70578d0db6ed13ccb03b78ba94d7fec5e93e48
Puneet Bajaj <puneetbajaj591@gmail.com>
2023-10-27 09:31:16 -0400

    downloads page and other small bug fixed. beta v1

--- Netflix_Clone/Controllers/Core/SearchViewController.swift
+++ Netflix_Clone/Controllers/Core/SearchViewController.swift
@@ -100,7 +100,7 @@ extension SearchViewController : UITableViewDataSource, UITableViewDelegate{
     
 }
 
-extension SearchViewController: UISearchResultsUpdating {
+extension SearchViewController: UISearchResultsUpdating, SearchResultsViewControllerDelegate {
     func updateSearchResults(for searchController: UISearchController) {
         let searchBar = searchController.searchBar
         
@@ -108,6 +108,7 @@ extension SearchViewController: UISearchResultsUpdating {
               !query.trimmingCharacters(in: .whitespaces).isEmpty,
               query.trimmingCharacters(in: .whitespaces).count >= 1,
               let resultsController = searchController.searchResultsController as? SearchResultsViewController else{return}
+        resultsController.delegate = self
         
         APICaller.shared.search(with: query) { result in
             DispatchQueue.main.async{
@@ -119,6 +120,14 @@ extension SearchViewController: UISearchResultsUpdating {
                     print(error.localizedDescription)
                 }
             }
+        }
+    }
+    func searchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel) {
+        DispatchQueue.main.async
+        {[weak self] in
+            let vc = TitlePreviewViewController()
+            vc.configure(with: viewModel)
+            self?.navigationController?.pushViewController(vc, animated: true)
         }
     }
 }
